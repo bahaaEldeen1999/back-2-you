7a0918174dde785192cd790ffc86d343dd6fa88c
bahaaEldeen1999 <bahaaeldeen1999@gmail.com>
2022-04-22 23:01:17 +0200

    fix jump forces

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,11 +1,11 @@
 extends KinematicBody2D
 
 export var MAX_SPEED = 405;
-export var GRAVITY = 500;
+export var GRAVITY = 100;
 export var MAX_JUMP_COUNT = 2;
 export var ACCELERATION = 300;
-export var JUMP_FORCE = [6000,8000];
-export var GRAVITY_INCREASE_FACTOR = 1.05;
+export var JUMP_FORCE = [300,250];
+export var GRAVITY_INCREASE_FACTOR = 1.1;
 var snap = true;
 var curr_jump_counter = 0;
 var velocity = Vector2.ZERO;
@@ -21,7 +21,7 @@ func _physics_process(delta):
 	position.x += velocity.x;
 	if (is_on_floor() || curr_jump_counter < MAX_JUMP_COUNT) && direction.y > 0:
 		snap = false;
-		velocity.y -= direction.y*delta*JUMP_FORCE[curr_jump_counter];
+		velocity.y -=  JUMP_FORCE[curr_jump_counter];
 		curr_jump_counter+=1;
 	elif !is_on_floor():
 		snap = false;
@@ -29,13 +29,13 @@ func _physics_process(delta):
 		gravity *= GRAVITY_INCREASE_FACTOR
 	var snap_vector =  Vector2(0, 64) if snap  else Vector2.ZERO;
 	move_and_slide_with_snap(velocity,snap_vector,Vector2(0,-1));
-	print(velocity.y)
+	#print(velocity.y)
 	if is_on_floor():
 		snap = true
 		curr_jump_counter = 0
 		velocity.y = 0
 		gravity = GRAVITY
-	print(curr_jump_counter)
+	#print(curr_jump_counter)
 	
 	
 func getPressDirection():
